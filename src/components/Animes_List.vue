32025c5e858b66dde979845d5c1039e9cb6b75dc
ricardo564 <ricardo_hks@hotmail.com>
2021-10-13 21:05:24 -0300

    first_commit
    Project for basic of PrimeFlex

---
+++ src/components/Animes_List.vue
@@ -0,0 +1,52 @@
+<template>
+<div  v-if="animes[0]">
+  <Card v-for="(anime, index) in animes" :key=index>
+    <template #title>
+      <div class="p-card-title text-primary">
+        {{ anime.anime }}
+      </div>
+    </template>
+    <template #content>
+      <div class="p-card-content">
+        {{ anime.quote }}
+      </div>
+    </template>
+    <template #footer>
+      <div class="p-card-footr text-primary">
+        {{ anime.character }}
+      </div>
+    </template>
+  </Card>
+</div>
+</template>
+
+<script>
+import Card from "primevue/card";
+import axios from "axios";
+
+export default {
+  name: "AnimesList",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      animes: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://animechan.vercel.app/api/quotes")
+      .then(({ data }) => {
+        this.animes = data;
+        console.log(this.animes);
+        console.log(this.animes[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<!-- CSS  -->
--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import PrimeVue from 'primevue/config';
+import "primeflex/primeflex.css"
 
-createApp(App).use(store).use(router).mount("#app");
+import "primevue/resources/themes/bootstrap4-dark-purple/theme.css"
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(PrimeVue)
+    .mount("#app");
