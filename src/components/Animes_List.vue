41a216b5bbe235b27c27eb32077c9ef666c5d7bf
ricardo564 <ricardo_hks@hotmail.com>
2021-10-14 08:50:16 -0300

    update2
    Adicionado atributos para margin e flex básico

--- src/components/Animes_List.vue
+++ src/components/Animes_List.vue
@@ -1,28 +1,35 @@
 <template>
-<div  v-if="animes[0]">
-  <Card v-for="(anime, index) in animes" :key=index>
-    <template #title>
-      <div class="p-card-title text-primary">
-        {{ anime.anime }}
-      </div>
-    </template>
-    <template #content>
-      <div class="p-card-content">
-        {{ anime.quote }}
-      </div>
-    </template>
-    <template #footer>
-      <div class="p-card-footr text-primary">
-        {{ anime.character }}
-      </div>
-    </template>
-  </Card>
-</div>
+  <h1 class="text-primary">Whispers From The Abyss</h1>
+  <div class="cursor-pointer">
+    <Carousel :value="cars">
+      <template #item="slotProps">
+        <Card class="m-1" v-for="(anime, index) in animes" :key="index">
+          <template #title>
+            <div class="p-card-title text-primary">
+              {{ anime.anime }}
+            </div>
+          </template>
+          <template #content>
+            <div class="p-card-content">
+              {{ anime.quote }}
+            </div>
+          </template>
+          <template #footer>
+            <div class="p-card-footr text-primary">
+              {{ anime.character }}
+            </div>
+          </template>
+        </Card>
+      </template>
+    </Carousel>
+  </div>
 </template>
 
 <script>
+import Carousel from 'primevue/carousel';
 import Card from "primevue/card";
 import axios from "axios";
+
 
 export default {
   name: "AnimesList",
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
 import PrimeVue from 'primevue/config';
 import "primeflex/primeflex.css"
 
